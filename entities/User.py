a2674a7a48136664148e0930316d5065d367d708
Kenay Perez <Kenaygp1105@gmail.com>
2026-03-03 21:43:04 -0700

    Base Class

--- entities/User.py
+++ entities/User.py
@@ -1,9 +1,16 @@
+from uuid import uuid4
 from sqlalchemy.orm import Mapped, mapped_column
 from fraccionamiento.database.database import Base
+from datetime import datetime
 
+class BaseClass(Base):
+    __abstract__ = True
+    id: Mapped[uuid4] = mapped_column(primary_key=True, index=True, default=uuid4)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
+    updated_at: Mapped[datetime] = mapped_column(default=datetime.now(), onupdate=datetime.now())
+    status: Mapped[bool] = mapped_column(default=True)
 
-class User(Base):
+class User(BaseClass):
     __tablename__ = 'users'
-    id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
     username: Mapped[str] = mapped_column(unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column()
