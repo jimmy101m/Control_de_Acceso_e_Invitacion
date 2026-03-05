43f4a67a07daa27f2c3b1bb78e0e5be2adc0cb9a
Jimmy <jagargo4@gmail.com>
2026-03-04 21:54:33 -0600

    Corriemdo Docker

--- database/database.py
+++ database/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://user:password@localhost:5432/fraccionamiento_db"
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
